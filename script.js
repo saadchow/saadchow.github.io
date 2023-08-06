fbabbab5e04010ab5b82e70c688fd6e2c6749ce7
saadchow <109829705+saadchow@users.noreply.github.com>
2023-08-06 16:17:11 -0400

    update website CSS/HTML

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
-const arrows = document.querySelectorAll('.arrow');
+let slideIndex = 1;
+showSlides(slideIndex);
 
-arrows.forEach(arrow => {
-    arrow.addEventListener('click', () => {
-        const currentSection = arrow.closest('.section');
-        const nextSection = currentSection.nextElementSibling;
-        if (nextSection) {
-            nextSection.scrollIntoView({ behavior: 'smooth' });
-        }
-    });
-});
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-$.scrollify({
-  section: '.section',
-  scrollSpeed: 1100,
-  easing: 'easeOutExpo',
-});
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+
